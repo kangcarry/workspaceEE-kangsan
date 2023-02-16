8e9e221bee3a18e3c4e545ecde46047abbd064c1
kangcarry <111220895+kangcarry@users.noreply.github.com>
2023-02-16 23:30:16 +0900

    ..

--- user_model2.0/src/main/java/com/itwill/user/controller/UserLoginActionController.java
+++ user_model2.0/src/main/java/com/itwill/user/controller/UserLoginActionController.java
@@ -4,28 +4,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.catalina.Session;
+
 import com.itwill.summer.mvc.Controller;
 import com.itwill.user.User;
 import com.itwill.user.UserService;
 
 public class UserLoginActionController implements Controller {
 	private UserService userService;
-	public UserLoginActionController() throws Exception{
-		userService=new UserService();
+	public UserLoginActionController() throws Exception {
+		userService = new UserService();
+		
 	}
-	@Override
+
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		/*
-		0  . GET방식요청일때 redirect:user_login_form.do forwardPath반환
-		2  . 파라메타 받기
-		3  . UserService객체생성
-		4  . UserService.login() 메쏘드실행
-		  0 : 아이디존재안함  	-> forward:/WEB-INF/views/user_login_form.jsp  forwardPath반환
-		  1 : 패쓰워드 불일치	-> forward:/WEB-INF/views/user_login_form.jsp  forwardPath반환
-		  2 : 로그인성공(세션)	-> redirect:user_main.do  forwardPath반환
-		*/
+		 * 0 . GET방식요청일때 redirect:user_login_form.do forwardPath반환 2 . 파라메타 받기 3 .
+		 * UserService객체생성 4 . UserService.login() 메쏘드실행 0 : 아이디존재안함 ->
+		 * forward:/WEB-INF/views/user_login_form.jsp forwardPath반환 1 : 패쓰워드 불일치 ->
+		 * forward:/WEB-INF/views/user_login_form.jsp forwardPath반환 2 : 로그인성공(세션) ->
+		 * redirect:user_main.do forwardPath반환
+		 */
 		String forwardPath = "";
-		
+		HttpSession session = request.getSession();
+		try {
+			if (request.getMethod().equalsIgnoreCase("GET")) {
+				forwardPath = "redirect:user_login_form.do";
+			} else {
+				String userId = request.getParameter("userId");
+				String password = request.getParameter("password");
+				int loginNo = userService.login(userId, password);
+				if (loginNo == 2) {
+					session.setAttribute("sUserId", userId);
+					forwardPath = "redirect:user_login_form.do";
+				} else {
+					forwardPath = "forward:/WEB-INF/views/user_login_form.jsp";
+				}
+			}
+		} catch (Exception e) {
+			forwardPath = "redirect:user_error.do";
+			e.printStackTrace();
+		}
 		return forwardPath;
 	}
 
--- user_model2.0/src/main/java/com/itwill/user/controller/UserViewController.java
+++ user_model2.0/src/main/java/com/itwill/user/controller/UserViewController.java
@@ -2,14 +2,17 @@ package com.itwill.user.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.itwill.summer.mvc.Controller;
 import com.itwill.user.User;
 import com.itwill.user.UserService;
 
 public class UserViewController implements Controller{
-	
-	@Override
+	private UserService userService;
+	public UserViewController() throws Exception {
+		userService = new UserService();
+	}
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		String forwardPath="";
 		/****************login_check*******************/
@@ -19,8 +22,16 @@ public class UserViewController implements Controller{
 		2. 세션의 sUserId를 사용해서 UserService.findUser()메쏘드호출
 		3. 반환된 User객체출력
 		*/
-		
-		
+		try{
+			HttpSession session = request.getSession();
+			String sUserId = (String)session.getAttribute("sUserId");
+			User user = userService.findUser(sUserId);
+			request.setAttribute("user", user);
+			forwardPath="forwardPath:/WEB-INF/views/user_view.jsp";
+		}catch (Exception e) {
+			e.printStackTrace();
+			forwardPath = "redirect:user_error_form.do";
+		}
 		return forwardPath;
 	}
 
